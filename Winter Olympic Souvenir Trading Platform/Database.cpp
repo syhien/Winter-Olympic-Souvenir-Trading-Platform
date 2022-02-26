0e2215b126c060a3843c3cb094597e4d344dc04d
syhien <syhien@outlook.at>
2022-02-26 11:28:23 +0800

    实现了所有select

--- Winter Olympic Souvenir Trading Platform/Database.cpp
+++ Winter Olympic Souvenir Trading Platform/Database.cpp
@@ -11,7 +11,21 @@
 #include <typeinfo>
 #include <iomanip>
 #include <ctime>
+#include <ranges>
+#include <Windows.h>
 using namespace std;
+
+wstring string2wstring(string str)
+{
+	wstring result;
+	int len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.size(), NULL, 0);
+	TCHAR* buffer = new TCHAR[len + 1];
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.size(), buffer, len);
+	buffer[len] = '\0';
+	result.append(buffer);
+	delete[] buffer;
+	return result;
+}
 
 Database::Database(std::vector<std::pair<std::string, std::string>> inputFiles)
 {
@@ -47,4 +61,71 @@ Database::Database(std::vector<std::pair<std::string, std::string>> inputFiles)
 			__table[tableName] = std::vector<std::vector<std::pair<std::string, std::string> > >();
 		}
 	}
+}
+
+std::vector<std::vector<std::pair<std::string, std::string> > > Database::perform(std::string command)
+{
+	istringstream commandStream(command);
+	string instruction;
+	commandStream >> instruction;
+	if (instruction == "SELECT") {
+		return __select(commandStream);
+	}
+	else if (instruction == "INSERT") {
+		return __insert(commandStream);
+	}
+	else if (instruction == "UPDATE") {
+		return __update(commandStream);
+	}
+	else {
+		throw "No selected command called " + instruction;
+	}
+	return std::vector<std::vector<std::pair<std::string, std::string> > >();
+}
+
+std::vector<std::vector<std::pair<std::string, std::string> > > Database::__select(std::istringstream& stream)
+{
+	string assertString;
+	stream >> assertString;
+	if (assertString != "*") {
+		throw "Wrong command format.";
+	}
+	stream >> assertString;
+	if (assertString != "FROM") {
+		throw "Wrong command format.";
+	}
+	string tableName;
+	stream >> tableName;
+	if (__table.find(tableName) == __table.end()) {
+		throw "No selected table named " + tableName;
+	}
+	if (!(stream >> assertString) or assertString.empty()) {
+		// select ALL
+		return __table[tableName];
+	}
+	else if (assertString == "WHERE") {
+		string column;
+		stream >> column;
+		stream >> assertString;
+		if (assertString != "CONTAINS") {
+			throw "Wrong command format.";
+		}
+		string value;
+		stream >> value;
+		if ((stream >> assertString) and !assertString.empty()) {
+			throw "Wrong command format.";
+		}
+		std::vector<std::vector<std::pair<std::string, std::string> > > result;
+		for (auto& i : __table[tableName]) {
+			for (auto& j : i | views::filter([column](const pair<string, string> k) {return k.first == column; })) {
+				if (string2wstring(j.second).find(string2wstring(value)) != wstring::npos)
+					result.push_back(i);
+			}
+		}
+		return result;
+	}
+	else {
+		throw "Wrong command format.";
+	}
+	return std::vector<std::vector<std::pair<std::string, std::string> > >();
 }
--- Winter Olympic Souvenir Trading Platform/Database.h
+++ Winter Olympic Souvenir Trading Platform/Database.h
@@ -11,8 +11,12 @@ class Database
 public:
 	Database(std::vector<std::pair<std::string, std::string> > inputFiles = { {"commodity","commodity.txt"},{"order","order.txt"},{"user","user.txt"}
 		});
+	std::vector<std::vector<std::pair<std::string, std::string> > > perform(std::string command);
 private:
 	std::map<std::string, std::vector<std::vector<std::pair<std::string, std::string> > > > __table;
 	std::map<std::string, std::vector<std::string> > __columnOfTable;
+	std::vector<std::vector<std::pair<std::string, std::string> > > __select(std::istringstream& stream);
+	std::vector<std::vector<std::pair<std::string, std::string> > > __insert(std::istringstream& stream);
+	std::vector<std::vector<std::pair<std::string, std::string> > > __update(std::istringstream& stream);
 };
 
