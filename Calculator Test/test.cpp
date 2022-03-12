a475321a319aecb9abbe83421c5d084368c74145
syhien <syhien@outlook.at>
2022-03-12 16:47:52 +0800

    增加计算器测试样例

--- Calculator Test/test.cpp
+++ Calculator Test/test.cpp
@@ -60,4 +60,5 @@ TEST(CalculatorTest, TestResult) {
 	EXPECT_EQ(calculator.perform("-2.05"), "-2.1");
 	EXPECT_EQ(calculator.perform("2.04"), "2");
 	EXPECT_EQ(calculator.perform("1.1+0.9"), "2");
+	EXPECT_EQ(calculator.perform("318-30-318*0.1-318*60/800"), "232.4");
 }
